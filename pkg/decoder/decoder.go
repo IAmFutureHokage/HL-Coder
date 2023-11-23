186cfe61709faf2fe4275a1f573852fb1e132ff1
IAmFutureHokage <doroshenkosa@outlook.com>
2023-11-23 21:03:19 +1000

    WaterLavelon20h and Temperature decoders

--- pkg/decoder/decoder.go
+++ pkg/decoder/decoder.go
@@ -27,6 +27,10 @@ func DateAndTimeDecode(s string) (*types.DateAndTime, error) {
 		return nil, err
 	}
 
+	if s[4] != '1' {
+		return nil, fmt.Errorf("fifth character must be '1'")
+	}
+
 	day, err := strconv.Atoi(s[:2])
 	if err != nil || day > 31 {
 		return nil, fmt.Errorf("invalid day value")
@@ -35,10 +39,6 @@ func DateAndTimeDecode(s string) (*types.DateAndTime, error) {
 	hour, err := strconv.Atoi(s[2:4])
 	if err != nil || hour > 23 {
 		return nil, fmt.Errorf("invalid hour value")
-	}
-
-	if s[4] != '1' {
-		return nil, fmt.Errorf("fifth character must be '1'")
 	}
 
 	return &types.DateAndTime{
@@ -56,6 +56,10 @@ func WaterLevelOnTime(s string) (*types.WaterLevelOnTime, error) {
 
 	if s[0] != '1' {
 		return nil, fmt.Errorf("first character must be '1'")
+	}
+
+	if s[1:] == "////" {
+		return nil, nil
 	}
 
 	waterlevel, err := strconv.Atoi(s[1:])
@@ -79,6 +83,10 @@ func DeltaWaterLevel(s string) (*types.DeltaWaterLevel, error) {
 		return nil, fmt.Errorf("first character must be '2'")
 	}
 
+	if s[1:] == "////" {
+		return nil, nil
+	}
+
 	if s[4] != '1' && s[4] != '2' {
 		return nil, fmt.Errorf("fifth character must be '1' or '2'")
 	}
@@ -99,16 +107,75 @@ func DeltaWaterLevel(s string) (*types.DeltaWaterLevel, error) {
 	}, nil
 }
 
+func WaterLevelOn20hDecoder(s string) (*types.WaterLevelOn20h, error) {
+
+	err := checkCodeBlock(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if s[0] != '3' {
+		return nil, fmt.Errorf("first character must be '3'")
+	}
+
+	if s[1:] == "////" {
+		return nil, nil
+	}
+
+	waterlevel, err := strconv.Atoi(s[1:])
+	if err != nil {
+		return nil, fmt.Errorf("invalid waterlavel value")
+	}
+
+	return &types.WaterLevelOn20h{
+		WaterLevel20h: uint16(waterlevel),
+	}, nil
+}
+
+func Temperature(s string) (*types.Temperature, error) {
+
+	err := checkCodeBlock(s)
+	if err != nil {
+		return nil, err
+	}
+
+	if s[0] != '4' {
+		return nil, fmt.Errorf("first character must be '4'")
+	}
+
+	if s[1:] == "////" {
+		return nil, nil
+	}
+
+	waterTemp, err := strconv.Atoi(s[1:3])
+	if err != nil {
+		return nil, fmt.Errorf("Ivalid water temperature value")
+	}
+
+	AirTemp, err := strconv.Atoi(s[3:])
+	if err != nil {
+		return nil, fmt.Errorf("Ivalid air temperature value")
+	}
+
+	if AirTemp > 50 {
+		AirTemp = 0 - AirTemp + 50
+	}
+
+	return &types.Temperature{
+		WaterTemperature: float32(waterTemp) / 10.0,
+		AirTemperature:   int8(AirTemp),
+	}, nil
+}
+
 func checkCodeBlock(s string) error {
 
-	matched, err := regexp.MatchString(`^\d{5}$`, s)
-
+	matched, err := regexp.MatchString(`^\d(////|\d{4})$`, s)
 	if err != nil {
 		return fmt.Errorf("error while matching regex: %v", err)
 	}
 
 	if !matched {
-		return fmt.Errorf("the string must be exactly 5 digits long")
+		return fmt.Errorf("the string must be exactly 5 characters long and consist of a digit followed by either four digits or four slashes")
 	}
 
 	return nil
